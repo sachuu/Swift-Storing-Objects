035fdaab09e76ae85af4116d13bdca883acc5eb0
sachintha herath <hera9030>
2020-01-28 19:10:57 -0500

    update

--- hera9030_a2/ViewController.swift
+++ hera9030_a2/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+var upCount = 0
+var downCount = 0
 
 var avacadoImage = "images/avacado"
 var bananaImage = "images/banana"
@@ -21,11 +23,40 @@ let tomato = Fruit(fruitName: "tomato", fruitImageName: tomatoImage)
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fruitView: UIImageView!
+    @IBOutlet weak var downLabel: UILabel!
+    @IBOutlet weak var upLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func nextPress(_ sender: Any) {
+        FruitCollection.current += 1
+        
+        if(FruitCollection.current == 0){
+            fruitView.image = UIImage(named: FruitCollection.collection[FruitCollection.current].fruitImageName)
+        }
+        if(FruitCollection.current == 1){
+             fruitView.image = UIImage(named: FruitCollection.collection[FruitCollection.current].fruitImageName)
+        }
+        if(FruitCollection.current == 2){
+             fruitView.image = UIImage(named: FruitCollection.collection[FruitCollection.current].fruitImageName)
+        }
+        if(FruitCollection.current == 3){
+            FruitCollection.current = 0
+        }
+    }
+    
+    @IBAction func upButtonPress(_ sender: Any) {
+        upCount += 1
+        upLabel.text = String(upCount)
+    }
+    
+    @IBAction func downButtonPress(_ sender: Any) {
+        downCount += 1
+        downLabel.text = String(downCount)
+    }
 }
-
